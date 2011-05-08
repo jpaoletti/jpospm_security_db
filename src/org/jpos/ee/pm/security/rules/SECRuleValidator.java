cf274cf533a1e8f63332e39bf232493ec0b49297
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-05-08 11:24:47 -0300

    Removed unimplemented security rules. I will add some decent rule system asap.

--- src/org/jpos/ee/pm/security/rules/SECRuleValidator.java
+++
@@ -1,7 +0,0 @@
-package org.jpos.ee.pm.security.rules;
-
-import org.jpos.ee.pm.security.SECException;
-
-public interface SECRuleValidator {
-    public void validate(String aplied, String param)throws SECException;
-}
