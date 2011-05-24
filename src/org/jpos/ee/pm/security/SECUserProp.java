e5292b1cfeade45296c8d4636c8f4803f923eee2
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-05-24 12:08:06 -0300

    Prop id is autogenerated

--- src/org/jpos/ee/pm/security/SECUserProp.java
+++ src/org/jpos/ee/pm/security/SECUserProp.java
@@ -29,6 +29,7 @@ import javax.persistence.*;
 public class SECUserProp implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @ManyToOne
     @JoinColumn(name="user")
